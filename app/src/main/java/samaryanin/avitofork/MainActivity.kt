78b18564d3f418c7871e34d2003d46d5c37d7bb4
Dimitri Simonyan <simadimonyan@gmail.com>
2025-01-10 15:17:11 +0300

    Start Workflow

--- app/src/main/java/samaryanin/avitofork/MainActivity.kt
+++ app/src/main/java/samaryanin/avitofork/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import samaryanin.avitofork.ui.theme.AvitoForkTheme
 
+//test
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
