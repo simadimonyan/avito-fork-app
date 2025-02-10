f33fcea386e8688acb053304567bac14c2a7eb9f
ptupi <gaaronk112@gmail.com>
2025-02-10 13:48:54 +0300

    marketplace screen

--- app/src/main/java/samaryanin/avitofork/presentation/screens/poster/MarketplaceScreen.kt
+++ app/src/main/java/samaryanin/avitofork/presentation/screens/poster/MarketplaceScreen.kt
@@ -1,9 +1,13 @@
 package samaryanin.avitofork.presentation.screens.poster
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Scaffold
@@ -12,9 +16,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import samaryanin.avitofork.R
 import samaryanin.avitofork.presentation.ui.components.utils.textField.AppTextFieldPlaceholder
 
 @Preview(showSystemUi = false)
@@ -28,18 +35,37 @@ fun MarketplaceScreen() {
     var search by remember { mutableStateOf("") }
 
     val ads = listOf(
-        Product("Laptop", "$1200", "New York", "https://via.placeholder.com/150"),
-        Product("Phone", "$800", "Los Angeles", "https://via.placeholder.com/150"),
-        Product("Bike", "$400", "Chicago", "https://via.placeholder.com/150"),
-        Product("Camera", "$600", "Miami", "https://via.placeholder.com/150"),
+        Product("мерседес S", "1200$", "New York", "url"),
+        Product("Phone", "800$", "Los Angeles", "url"),
+        Product("Bike", "400$", "Chicago" , "url"),
+        Product("Camera", "600$", "Miami", "url"),
     )
 
     Scaffold { paddingValues ->
         Column(modifier = Modifier.fillMaxSize()) {
-            Row(modifier = Modifier.padding(8.dp)){
-                AppTextFieldPlaceholder(placeholder = "Поиск", value = search,
+            Box(modifier = Modifier.padding(8.dp), contentAlignment = Alignment.CenterEnd) {
+                AppTextFieldPlaceholder(
+                    placeholder = "Поиск", value = search,
                     onValueChange = { search = it }, isPassword = false, errorListener = false
                 )
+                Row (modifier = Modifier.padding(end = 8.dp)){
+                    Image(
+                        modifier = Modifier.
+                        clickable {  }
+                            .size(24.dp)
+                            .padding(end = 8.dp),
+                        painter = painterResource(R.drawable.search),
+                        contentDescription = ""
+                    )
+                    Image(
+                        modifier = Modifier.
+                        clickable {  }
+                            .size(24.dp),
+                        painter = painterResource(R.drawable.filter),
+                        contentDescription = ""
+                    )
+                }
+
             }
             LazyVerticalGrid(
                 columns = GridCells.Adaptive(minSize = 150.dp),
@@ -50,6 +76,5 @@ fun MarketplaceScreen() {
                 }
             }
         }
-
     }
 }
--- app/src/main/java/samaryanin/avitofork/presentation/screens/poster/ProductCard.kt
+++ app/src/main/java/samaryanin/avitofork/presentation/screens/poster/ProductCard.kt
@@ -1,13 +1,15 @@
 package samaryanin.avitofork.presentation.screens.poster
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -17,8 +19,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import samaryanin.avitofork.R
+
+@Preview(showSystemUi = false)
+@Composable
+fun ProductCardScreenPreview() {
+    ProductCard(
+        Product("мерседес S-19302-s 860", "1200$", "New York", "url"),
+    )
+}
+
 
 @Composable
 fun ProductCard(product: Product) {
@@ -28,20 +40,55 @@ fun ProductCard(product: Product) {
             .aspectRatio(1f),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
-        Column(
-            horizontalAlignment = Alignment.Start,
-            modifier = Modifier.padding(8.dp).fillMaxSize()
-        ) {
+//        Column(
+//            horizontalAlignment = Alignment.Start,
+//            modifier = Modifier
+//                .fillMaxSize()
+//        ) {
             Image(
-                painter = painterResource(R.drawable.ic_pass_view),
+                painter = painterResource(R.drawable.car),
                 contentDescription = null,
-                modifier = Modifier.fillMaxWidth().weight(1f),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(.9f),
                 contentScale = ContentScale.Crop
             )
             Spacer(modifier = Modifier.height(8.dp))
-            Text(product.title, style = MaterialTheme.typography.bodyLarge)
-            Text(product.price, style = MaterialTheme.typography.titleLarge)
-            Text(product.location, style = MaterialTheme.typography.bodySmall)
-        }
+            Row(modifier = Modifier) {
+                Column(
+                    horizontalAlignment = Alignment.Start,
+                    modifier = Modifier
+                        .weight(.8f)
+                        .padding(8.dp)
+                ) {
+                    Text(product.title, style = MaterialTheme.typography.bodyLarge, softWrap = true, maxLines = 1)
+                    Text(product.price, style = MaterialTheme.typography.titleLarge, softWrap = true, maxLines = 1)
+                    Text(product.location, style = MaterialTheme.typography.bodySmall, softWrap = true, maxLines = 1)
+                }
+                Column(
+                    horizontalAlignment = Alignment.End,
+                    modifier = Modifier
+                        .weight(.2f)
+                        .padding(8.dp)
+                ) {
+                    Image(
+                        modifier = Modifier.
+                        clickable {  }
+                            .size(24.dp),
+                        painter = painterResource(R.drawable.like_non_act),
+                        contentDescription = ""
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Image(
+                        modifier = Modifier.
+                        clickable {  }
+                            .size(24.dp),
+                        painter = painterResource(R.drawable.more),
+                        contentDescription = ""
+                    )
+                }
+            }
+
+       // }
     }
 }
