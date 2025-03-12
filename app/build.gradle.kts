8fdd80943ba2eae3d980821cb8c69af87efbdd53
ptupi <gaaronk112@gmail.com>
2025-03-12 15:25:26 +0300

    room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,10 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
     implementation(libs.androidx.material.icons.extended)
 
     implementation(libs.coil.compose)
