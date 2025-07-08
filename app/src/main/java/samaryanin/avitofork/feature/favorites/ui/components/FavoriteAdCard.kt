1f808ea76eb94a3594316fdafdc5c8ca8a88a189
ptupi <gaaronk112@gmail.com>
2025-07-08 23:17:41 +0300

    favoriteScreen ui

--- app/src/main/java/samaryanin/avitofork/feature/favorites/ui/components/FavoriteAdCard.kt
+++ app/src/main/java/samaryanin/avitofork/feature/favorites/ui/components/FavoriteAdCard.kt
@@ -1,6 +1,5 @@
 package samaryanin.avitofork.feature.favorites.ui.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,12 +13,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -27,10 +28,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import samaryanin.avitofork.R
-import samaryanin.avitofork.shared.ui.components.RemoteImage
 import samaryanin.avitofork.feature.favorites.domain.models.Ad
-import samaryanin.avitofork.feature.feed.ui.navigation.NavigationHolder
 import samaryanin.avitofork.feature.feed.ui.navigation.FeedRoutes
+import samaryanin.avitofork.feature.feed.ui.navigation.NavigationHolder
+import samaryanin.avitofork.shared.extensions.formatPrice
+import samaryanin.avitofork.shared.ui.components.RemoteImage
 
 // Компонент карточки объявления
 @Composable
@@ -43,54 +45,63 @@ fun FavoriteAdCard(
     Card(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(horizontal = 12.dp, vertical = 6.dp)
             .clickable {
-                globalNavController.navigate(FeedRoutes.AdditionalInfoScreen.route)
                 NavigationHolder.id = ad.id
+                globalNavController.navigate(FeedRoutes.AdditionalInfoScreen.route)
             },
         colors = CardDefaults.cardColors(
-            containerColor = Color.Transparent,
-            contentColor = MaterialTheme.colorScheme.onBackground
-        )
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface
+        ),
+        elevation = CardDefaults.cardElevation(4.dp),
+        shape = RoundedCornerShape(12.dp)
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp)
+                .padding(12.dp)
         ) {
-            // Загрузка изображения с shimmer-анимацией
+            // Фото
             RemoteImage(
                 imageId = ad.imageIds.firstOrNull().orEmpty(),
                 modifier = Modifier
-                    .size(170.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .align(Alignment.CenterVertically)
+                    .size(120.dp)
+                    .clip(RoundedCornerShape(10.dp)),
+                contentScale = ContentScale.Crop
             )
 
-            Spacer(modifier = Modifier.width(16.dp))
+            Spacer(modifier = Modifier.width(12.dp))
 
-            // Текстовая информация
+            // Основная информация
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .align(Alignment.Top)
+                    .align(Alignment.CenterVertically)
             ) {
                 Text(
                     text = ad.title,
-                    fontSize = 18.sp,
+                    fontSize = 24.sp,
                     fontWeight = FontWeight.Bold,
                     maxLines = 2,
                     overflow = TextOverflow.Ellipsis
                 )
-                Spacer(modifier = Modifier.height(4.dp))
+
+                Spacer(modifier = Modifier.height(6.dp))
+
                 Text(
-                    text = ad.price,
-                    fontSize = 16.sp
+                    text = ad.price.formatPrice(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Normal,
+               //     color = MaterialTheme.colorScheme.primary
                 )
-                Spacer(modifier = Modifier.height(4.dp))
+
+                Spacer(modifier = Modifier.height(6.dp))
+
                 Text(
                     text = ad.address,
-                    fontSize = 14.sp,
-                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f),
                     maxLines = 2,
                     overflow = TextOverflow.Ellipsis
                 )
@@ -98,9 +109,11 @@ fun FavoriteAdCard(
 
             Spacer(modifier = Modifier.width(8.dp))
 
-            Image(
-                painter = painterResource(if (isFavorite) R.drawable.like_act else R.drawable.like_non_act),
+            // Иконка избранного
+            Icon(
+                painter = painterResource(id = R.drawable.like_act),
                 contentDescription = null,
+                tint = Color.Unspecified, // <-- добавь это
                 modifier = Modifier
                     .size(24.dp)
                     .clickable { onLikeClick(ad) }
